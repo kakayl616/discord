41cc8a245d4bbddc4d92434dfc59355a29e30704
kakayl616 <kaylsmith27@gmail.com>
2025-02-07 09:52:53 +0800

    new commit from discord-chat

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#5865f2" />
+      <title>User Information Form</title>
+    </>
+  );
+}
