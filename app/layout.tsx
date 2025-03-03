e1a8dc8a99c76f085ab96dcf104c3ef99f1fb890
Kakayl <kaylsmith27@gmail.com>
2025-03-03 16:40:50 +0800

    logs panel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
+import "./globals.css";
+
 export const metadata = {
-  title: "Discord Ban Appeal",
-  icons: {
-    icon: "/img/discord.ico",
-  },
+  title: "Discord Case Appeal",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
         <meta name="theme-color" content="#5865f2" />
+        {/* Put any global <title>, <meta> etc. here, or in `metadata` above */}
       </head>
       <body style={{ margin: 0, padding: 0 }}>
         {children}
