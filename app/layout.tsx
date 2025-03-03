11a4fffba438b3ee4a033fe075bddffc7c9d724a
Kakayl <kaylsmith27@gmail.com>
2025-03-03 15:46:49 +0800

    title icon 99

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,16 @@
-import "./globals.css";
-
 export const metadata = {
   title: "Discord Ban Appeal",
   icons: {
-    icon: "/img/discord.png",
+    icon: "/img/discord.ico",
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#5865f2" />
+      </head>
       <body style={{ margin: 0, padding: 0 }}>
         {children}
       </body>
